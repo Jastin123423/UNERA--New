101250f088492f1aeb1eda7b7c48e42d71a564b3
Jastin123423 <Jjovinatha@gmail.com>
2026-09-11 20:08:43 +0300

    build: update production assets
    Rebuild application assets and update main entry point to reference new build artifacts.

--- android/app/src/main/assets/public/assets/web-D3LA1icc.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-hqVsQMT7.js";import"https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";class i extends n{async show(e){}async hide(e){}}export{i as SplashScreenWeb};
